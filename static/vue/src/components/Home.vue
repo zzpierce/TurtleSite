2d22d50a7e1025f227d34a24f189d0751f263979
zhanzong <zhanzong@creditease.cn>
2016-12-23 02:24:31 +0800

    write personal page

--- static/vue/src/components/Home.vue
+++ static/vue/src/components/Home.vue
@@ -1,14 +1,80 @@
 <template>
-  <div>
-    <div>Home sweet home..</div>
+  <div class="container" v-bind:style="bgAttr">
+    <div class="title" >
+      <ul>
+        <li @mouseover="fancy(1)">个</li>
+        <li @mouseover="fancy(2)">人</li>
+        <li @mouseover="fancy(3)">主</li>
+        <li @mouseover="fancy(0)">页</li>
+      </ul>
+    </div>
+    <div class="signature">zz</div>
   </div>
 </template>
 <script>
+
   export default {
-    events: {
-      'hook:created': function() {
-        alert('Home haha Home');
+    data: function() {
+      return {
+        value: "123",
+        bgAttr: {
+          "background-color": "darkkhaki"
+        },
+        colors: [
+          "cadetblue",
+          "bisque",
+          "darkgray",
+          "coral"
+        ]
+      };
+    },
+    methods: {
+      fancy(v) {
+        this.bgAttr["background-color"] = this.colors[v];
       }
     }
   };
 </script>
+<style>
+  html, body {
+    font-size: 12px;
+    font-family: "Microsoft YaHei",Arial,Helvetica,sans-serif;
+    background-color: darkkhaki;
+    color: #222;
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  .container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+
+  .container .title {
+    font-size: 10em;
+    text-align: center;
+    font-family: "Microsoft JhengHei", sans-serif;
+    padding-top: 4%;
+    padding-bottom: 4%;
+  }
+
+  .title ul {
+    padding: 0;
+  }
+
+  .title ul>li {
+    display: inline-block;
+  }
+
+  .title ul>li:hover {
+    cursor: pointer;
+    color: white;
+  }
+
+  .container .signature {
+    position: absolute;
+    bottom: 0;
+  }
+</style>
--- static/vue/src/main.js
+++ static/vue/src/main.js
@@ -9,24 +9,12 @@ import About from './components/About.vue'
 import Crash_0 from './components/Crash_0.vue'
 
 const router = new VueRouter({
-  mode: 'history',
-  base: __dirname,
   routes: [
-    { path: '/', component: Home},
+    { path: '/home', component: Home},
     { path: '/about', component: About },
+    { path: '/**', component: Home },
     { path: '/crash_0', component: Crash_0}
   ]
 });
 
 new Vue(Vue.util.extend({ router }, App)).$mount('#app');
-
-
-
-// import Test from './Test.vue'
-//
-// new Vue({
-//   el: 'body',
-//   components: {
-//     Test: Test
-//   }
-// });
