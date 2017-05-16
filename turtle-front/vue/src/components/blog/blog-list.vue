38ec04a5ef4d0279ef6bb53baddb877885f18a90
pierce <zzpierce@163.com>
2017-05-17 02:05:52 +0800

    make change

--- turtle-front/vue/src/components/blog/blog-list.vue
+++ turtle-front/vue/src/components/blog/blog-list.vue
@@ -1,14 +1,43 @@
 <template>
   <div class="main">
+    <my-header></my-header>
     <ul>
       <li v-for="blog in blogList">
-        {{blog.title}}
+        <span>{{blog.title}}</span>
+        <span>{{blog.createTime}}</span>
       </li>
     </ul>
   </div>
 </template>
+<style scoped>
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  .main {
+    width: 90%;
+    margin: auto;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+  ul > li {
+    color: darkcyan;
+    font-size: 1.2em;
+  }
+
+  li > span:last-child {
+    float: right;
+  }
+</style>
 <script>
   import {DEV_URL} from '../../util/constants';
+  import MyHeader from '../util/header.vue';
 
   export default {
     data() {
@@ -24,11 +53,15 @@
         this.$http.get(DEV_URL.LOAD_BLOG_LIST)
           .then(res => {
             this.blogList = res.data;
+            console.log(this.blogList);
           })
           .catch(res => {
             console.log("error happens when load blogs.");
           });
       }
+    },
+    components: {
+      "my-header": MyHeader
     }
   }
 </script>
---
+++ turtle-front/vue/src/components/util/header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="my-header">
+    <div class="icon-span">T</div>
+    <div class="menu-div">
+      <ul class="header-menu">
+        <li>人生规划</li>
+        <li>技术总结</li>
+        <li>瞎JB写</li>
+      </ul>
+    </div>
+    <div class="log-div">
+      zzpierce
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .my-header {
+    margin-top: 0;
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+    background-color: cadetblue;
+    color: white;
+  }
+
+  .icon-span {
+    font-size: 1.7em;
+    color: white;
+    margin-left: 0.5em;
+  }
+
+  .header-menu {
+    list-style: none;
+  }
+
+  .log-div {
+    margin: 16px;
+    float: right;
+  }
+
+  .my-header div, li{
+    display: inline-block;
+  }
+</style>
+<script>
+  export default {
+    data() {
+      return {
+        userName: "zz"
+      }
+    }
+  }
+</script>
