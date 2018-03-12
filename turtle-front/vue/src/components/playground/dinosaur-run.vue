be3925d38219ab3b5832bb22c89b964eeed45454
zhanzong <zhanzong@creditease.cn>
2018-03-12 18:56:31 +0800

    dinosaur

--- turtle-front/vue/src/components/playground/dinosaur-run.vue
+++ turtle-front/vue/src/components/playground/dinosaur-run.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <canvas id="drawing" width="600px" height="400px"></canvas>
+    <canvas id="drawing"></canvas>
   </div>
 </template>
 <script>
@@ -9,12 +9,22 @@
     data() {
       return {
         ctx: "",
-        x: 0,
-        y: 0
+        position: {
+          x: 0,
+          y: 0
+        },
+        speed: {
+          xs: 3,
+          ys: 0
+        },
+        g: 0.4,
+        side: 50
       }
     },
     mounted() {
       let canvas = document.querySelector('canvas');
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
       this.ctx = canvas.getContext('2d');
       this.draw();
     },
@@ -22,12 +32,21 @@
       draw() {
         this.ctx.fillStyle = 'green';
         this.ctx.lineWidth = 0.3;
-        setInterval(this.move, 20);
+        setInterval(this.move, 5);
       },
       move() {
-        this.x += 1;
-        this.y += 1;
-        this.ctx.fillRect(this.x, this.y, 50, 50);
+        this.ctx.clearRect(this.position.x - 100, this.position.y - 100, this.side + 200, this.side + 200);
+        this.speed.ys += this.g;
+        this.position.x += this.speed.xs;
+        this.position.y += this.speed.ys;
+        this.ctx.fillRect(this.position.x, this.position.y, this.side, this.side);
+        if (this.position.y > window.innerHeight) {
+          this.position.y = window.innerHeight;
+          this.speed.ys *= -1;
+        }
+        if (this.position.x < 0 || this.position.x > window.innerWidth) {
+          this.speed.xs *= -1;
+        }
       }
     }
   }
